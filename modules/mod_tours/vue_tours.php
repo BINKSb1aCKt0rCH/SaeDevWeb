e72c15306d508bb51c4aa595deaf976712b0035a
zgueddou <zgueddou@iut.univ-paris8.fr>
2024-01-25 18:20:28 +0100

    tours

--- modules/mod_tours/vue_tours.php
+++ modules/mod_tours/vue_tours.php
@@ -3,40 +3,18 @@
 require_once 'vue_generique.php';
 
 class VueTours extends VueGenerique{
-/*
-    public function getNom($nom){
-        echo $nom['nomTour'];
-    }
 
-    public function getPuissance($puissance){
-        echo $puissance['puissance'];
-    }
-
-    public function getPrix($prix){
-        echo $prix['prix'];
-    }
-
-    public function getPortee($portee){
-        echo $portee['portee'];
-    }
-
-    public function getImage($image){
-        echo '<img src="images/'.$image.'"/>';
-    }
-*/
     public function afficheTours($tab){
         echo '
         <head>
         <link rel="stylesheet" href="styleTour.css">
         </head>
         <h1>Les Tours</h1>
-        <table> 
-            <tbody>
-                <tr>
+        <div class="card-container">
         ';
         foreach ($tab as $tours){
             echo '
-                <td class="case">
+                <div class="card">
                     <img src="'.$tours['image'].'">
                     <ul>
                         <li class="gray"> nom : '.$tours['nomTour'].'</li>
@@ -44,15 +22,12 @@ class VueTours extends VueGenerique{
                         <li class="gray"> prix : '.$tours['prix'].'</li>
                         <li class="blanc"> portée : '.$tours['portee'].'</li>
                     </ul>
-                </td>';
+                </div>';
         }
 
         echo '
-                </tr>
-            </tbody>
-            </table>
+            </div>
             ';
     }
-
 }
 ?>
